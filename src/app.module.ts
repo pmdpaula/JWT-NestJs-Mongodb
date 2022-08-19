240ba7e9fc86d765f631734173236f9a90ad95fa
Pedro de Paula (remoto) <pmdpaula@yahoo.com.br>
2022-08-19 20:47:12 -0300

    feat(package): adição do pacote mongoose
    Adicionado o pacote mongoose para conexão com banco de dados MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
+import * as dotenv from 'dotenv';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
+dotenv.config();
+
 @Module({
-  imports: [TasksModule],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.ae5wj.mongodb.net/?authSource=${process.env.MONGODB_DATABASE}&retryWrites=true&w=majority`,
+    ),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
